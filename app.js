241a67eedae329f90c2f5444b1541453ced91a75
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-21 11:29:58 -0500

    Variables and Data Types

---
+++ app.js
@@ -0,0 +1,21 @@
+var message = "hello!";
+message = "Goodbye!";
+message = "See you later!";
+console.log(message);
+
+var something;
+console.log(typeof something);
+
+something = "Hello!";
+console.log(typeof something);
+
+something = 78;
+console.log(typeof something);
+
+var number1 = 2;
+var number2 = 3;
+console.log(number1 + number2); // logs 5
+
+var string1 = '2';
+var string2 = '3';
+console.log(string1 + string2); // logs 23
